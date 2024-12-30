b8189de524d727393b6076f706b51e21a82ff649
Yeabsira <yabahane@gmail.com>
2024-12-30 14:28:44 +0300

    feat: heroImage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const page = () => {
   return (
-    <div>
-      <h1>Search</h1>
+    <div className="bg-[url('/images/hero-image.png')] h-screen w-screen  bg-contain bg-center bg-no-repeat flex items-center justify-center flex-col">
+      <h1 >Search</h1>
       <p>Search high-resolution images from Unsplash</p>
       <Input placeholder="Enter your keywords..." />
     </div>
